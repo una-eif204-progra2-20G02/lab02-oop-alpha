8ca196c17dc30b4c8ab57d6f7397378ba49fdc21
JordanJazz2430 <68917836+JordanJazz2430@users.noreply.github.com>
2020-08-04 12:18:28 -0600

    Constructores implementados, class Unversity

--- University.cpp
+++ University.cpp
@@ -4,3 +4,25 @@
 
 #include <sstream>
 #include "University.h"
+University::University(){
+
+}
+University::University(string name){
+    this->name=name;
+}
+University::University(string name, class Professor prof ,  class Administrative admini){
+    this->name=name;
+    this->Professor=prof;
+   // this->Administrative=admini;
+
+}
+string University::getName(){return this->name;}
+Professor University::getProfessor();
+Administrative University::getAdministrative();
+void University::setName(string);
+void University::setProfessor(Professor);
+void University::setAdministrative(Administrative);
+void University::addProfessor();
+void University::addAdministrative();
+string University::showProfessorList();
+string University::showAdministrativeList();
