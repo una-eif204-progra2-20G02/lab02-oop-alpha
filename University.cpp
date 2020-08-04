1167adac98e1d1b88bde614fbec41f22b5a86b2c
ronich06 <48327426+ronich06@users.noreply.github.com>
2020-08-04 12:22:04 -0600

    University.cpp

--- University.cpp
+++ University.cpp
@@ -4,3 +4,32 @@
 
 #include <sstream>
 #include "University.h"
+University::University(){
+    name = "";
+}
+University::University(string);
+University::University(string, Professor, Administrative);
+string University::getName();
+Professor University::getProfessor(){
+    return this->Professor;
+}
+Administrative University::getAdministrative();
+{
+    return this->Administrative;
+}
+void University::setName(string name);
+{
+    this->name = name;
+}
+void University::setProfessor(Professor Professor){
+    this->Professor = Professor
+}
+void University::setAdministrative(Administrative administrative){
+this->Administrative = administrative
+}
+void University::addProfessor(){
+    professorList->
+}
+void University::addAdministrative();
+string University::showProfessorList();
+string University::showAdministrativeList();
