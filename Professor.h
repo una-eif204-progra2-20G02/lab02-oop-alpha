d4f36268c80e3de0244633b393e990bbc2bc94ce
JordanJazz2430 <68917836+JordanJazz2430@users.noreply.github.com>
2020-08-04 11:32:23 -0600

    Clase .h Professor

--- Professor.h
+++ Professor.h
@@ -7,7 +7,21 @@
 #include "Person.h"
 
 class Professor: public Person {
+private:
+    double monthlySalary;
+    double commisionRate;
+public:
+    Professor();
+    Professor(double,double); //salary, commision
+    Professor(const std::string, const std::string, int, double,double); //noombre, apellido, Id,Salario, comiision
+    virtual ~Professor();
+    double getMonthlySalary();
+    double setMonthlySalary(double);
 
+    double getCommissionRate();
+    double setCommissionRate(double);
+    double const salary();
+    std::string const toString();
 };
 
 #endif //LAB02_OOP_PROFESSOR_H
