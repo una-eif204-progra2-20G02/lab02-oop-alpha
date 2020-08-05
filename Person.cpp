5ce31aaf9dd1fe7c664199f5872e6a313ddd5dd5
steveMadrigal <68918625+steveMadrigal@users.noreply.github.com>
2020-08-05 15:48:28 -0600

    Compiling File. With out comments.

--- Person.cpp
+++ Person.cpp
@@ -1,34 +1,55 @@
-//
-// Created by Maikol Guzman on 8/2/20.
-//
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   Person.cpp
+ * Author: computer
+ *
+ * Created on August 5, 2020, 10:29 AM
+ */
+
 
 #include "Person.h"
-Person::Person(){
-    firstName = "";
-    lastName = "";
-    documentId = 0;
+Person::Person():
+        _firstName(""),
+        _lastName (""),
+        _documentId(0)
+{
 }
-Person::Person(string firstName,string lastName,int documentId){
-    this->firstName = firstName;
-    this->lastName = lastName;
-    this->documentId = documentId;
+Person::Person(std::string firstName,std::string lastName,int documentId):
+        _firstName(firstName),
+        _lastName(lastName),
+        _documentId(documentId)
+{
 }
-Person::~Person(){}
-string Person::getFirstName(){
-    return this->firstName;
+Person::~Person(){
+
 }
-string Person::getLastName(){
-    return this->lastName;
+std::string Person::getFirstName(){
+    return _firstName;
+}
+std::string Person::getLastName(){
+    return _lastName;
 }
 int Person::getDocumentId(){
-    return this->documentId;
+    return _documentId;
 }
-void Person::setFirstName(string firstName){
-    this->firstName = firstName;
+void Person::setFirstName(std::string firstName){
+    _firstName = firstName;
 }
-void Person::setLastName(string lastName){
-    this->lastName = lastName;
+void Person::setLastName(std::string lastName){
+    _lastName = lastName;
 }
 void Person::setDocumentId(int documentId){
-    this->documentId = documentId;
+    _documentId = documentId;
+}
+std::string Person::toString()const{
+    std::stringstream s;
+    s<<"Name: "<<_firstName<<"\n";
+    s<<"Last Name: "<<_lastName<<"\n";
+    s<<"Document ID: "<<_documentId<<"\n";
+    return s.str();
 }
