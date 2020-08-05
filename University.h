cb40e43ce4249576b01a03149a06f61d24484965
steveMadrigal <68918625+steveMadrigal@users.noreply.github.com>
2020-08-05 15:57:49 -0600

    Compiling File. With out comments.v02.22

--- University.h
+++ University.h
@@ -1,37 +1,51 @@
-//
-// Created by Maikol Guzman on 8/2/20.
-//
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   University.h
+ * Author: computer
+ *
+ * Created on August 5, 2020, 12:54 PM
+ */
 
 #ifndef LAB02_OOP_UNIVERSITY_H
 #define LAB02_OOP_UNIVERSITY_H
-
-#include <vector>
 #include "Professor.h"
 #include "Administrative.h"
-#include "Lista.h"
-using namespace std;
+#include "Coleccions.h"
 class University {
-private:
-    string name;
-    Professor Professor;
-    Administrative Administrative;
-    Lista professorList;
-    Lista administrativeList;
 public:
     University();
-    University(string);
-    University(string, Professor, Administrative);
-    string getName();
-    Professor getProfessor();
-    Administrative getAdministrative();
-    void setName(string);
-    void setProfessor(Professor);
-    void setAdministrative(Administrative);
-    void addProfessor();
-    void addAdministrative();
-    string showProfessorList();
-    string showAdministrativeList();
+    University(const std::string);
+    University(const std::string ,Professor* const, Administrative* const );
+    virtual ~University();
+
+    std::string getName();
+    void setName(std::string);
+
+    Professor* getProfessor();
+    void setProfessor(Professor* const);
+
+    Administrative* getAdministrative();
+    void setAdministrative(Administrative* const);
+
+    void addProfessor(Professor*);
+    Coleccions::List<Professor>* getListProfessor();
+
+    void addAdministrative(Administrative*);
+    Coleccions::List<Administrative>* getListAdministrative();
+
+private:
+    std::string _name;
+    Professor* _professor;
+    Administrative* _administrative;
+    Coleccions::List<Professor>* _professorList=new Coleccions::List<Professor>();
+    Coleccions::List<Administrative>* _administrativeList=new Coleccions::List<Administrative>();
+
 };
 
+#endif /* LAB02_OOP_UNIVERSITY_H */
 
-#endif //LAB02_OOP_UNIVERSITY_H
